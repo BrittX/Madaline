7ae7d93192cfde2c50c7da70624e396672cdf104
BrittX <brittxbarnes@gmail.com>
2017-03-21 21:53:03 -0700

    MASSIVE UPDATES - mainly added threshold
    Implemented a threshold features that the user inputs (need to make sure
    they don't input 0 (?)) Also added a function that will calculate the
    changes to the weights/bias so I can use that to determine if the change
    will be within the threshold. Updated some of the helper functions to
    InitializeIT and testIt(). Renamed a couple functions, and implemented a
    function errorDiff to get threshold. I think that's it, other than
    deleting old print statements/adding new ones

--- madaline.py
+++ madaline.py
@@ -1,5 +1,8 @@
 """
 File for the madaline neural network
+
+Ask user input for threshold (which is the error)
+Check if weights have changed within that threshold then we say it's covnerged
 """
 import madmenu as mm
 import random as r
@@ -29,6 +32,11 @@ class Training(object):
 			self.bias = [round(r.uniform(-0.5, 0.5), 2) for x in range(3)]
 
 """
+Function to calculate the change in the weights
+"""
+def upWeights(rate, one, zin, x):
+	return round(rate * (one - zin) * x, 2)
+"""
 Run the training algorithm for Madaline
 
 args:
@@ -40,8 +48,9 @@ args:
 	pairs: Number of training pairs in the set
 	inputs: Number of inputs in each training set
 	outfile: output file to save the weights to
+	threshold: The value for checking if we have an error
 """
-def trainAlgo(tsets, bias, rate, epochs, weights, pairs, inputs, outfile, outputs):
+def trainAlgo(tsets, bias, rate, epochs, weights, pairs, inputs, outfile, threshold):
 	# Hardcode value for  for v1, v2 bias
 	v1 = v2 = bias[2] = .5
 	x = [] # to store x inputs
@@ -49,8 +58,9 @@ def trainAlgo(tsets, bias, rate, epochs, weights, pairs, inputs, outfile, output
 	stop = False # for determining convergence
 	a, b, c = 0, 1, 2 # for the bias/weights
 	count = 0 # keep track of number of inputs we've done so far
-	era = 0 # Keep track of number of epochs
-	converged = 0 # Check if we've converged
+	era = 1 # Keep track of number of epochs
+	temp = []
+	converged = 0
 
 	# Store each training pair, s:t 
 	for i,tset in enumerate(tsets):
@@ -61,8 +71,10 @@ def trainAlgo(tsets, bias, rate, epochs, weights, pairs, inputs, outfile, output
 	while not stop:
 		# Get the input to the hidden layer
 		for i in range(pairs):
+			print('\nThis weights for each round are: ', weights)
+			print('This is epoch: ', era)
 			count += 1
-			z_in1 = round(bias[a] + (x[i][a] * weights[a][a]) + (x[i][b] * weights[b][a]), 2) # Need to move to for loop(?)
+			z_in1 = round(bias[a] + (x[i][a] * weights[a][a]) + (x[i][b] * weights[b][a]), 2)
 			z_in2 = round(bias[b] + (x[i][a] * weights[a][b]) + (x[i][b] * weights[b][b]), 2)
 			print('This is z_in1: ', z_in1)
 			print('This is z_in2: ', z_in2)
@@ -75,82 +87,214 @@ def trainAlgo(tsets, bias, rate, epochs, weights, pairs, inputs, outfile, output
 			
 			# Get y = f(y_in)
 			y = float(activateF(y_in))
+			print('This is t', t[i][a])
 			
 			# Check if error occured
 			if t[i][a] == y: # Gets corresponding output
 				print('t == y') 
-				change = False
-				converged +=1 # increment converged
-				print('Converged equals ', converged)
-				# Check if we've converged
-				if converged >= pairs: # Means we haven't updated weights for each pair
-					print('Training converged after {x} epochs'.format(x=era+1))
+				converged += 1 # Increment converged
+				print('converged equals: ', converged)
+				if converged == pairs:
+					print('We have converged')
 					stop = True
+					print('Training converged after {x} epochs'.format(x=era))
 					# Write to output file
 					with open(outfile, "w") as store:
-						store.write("%d\n%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, outputs, pairs, weights, bias, v1, v2))
+						store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
 					store.close()
-					return outfile
 					break
 				continue
 			# t = 1 and z1/z2 = -1
-			elif t[i][a] == 1.0 and (z1 == -1 and z2 == -1): # check if t = 1 and z1/z2 = -1
+			elif t[i][a] == 1.0 and z1 == -1 or z2 == -1: # check if t = 1 and z1/z2 = -1
 				val = min((z_in1, z_in2), key=lambda x: abs(x - 0)) # Get z value closest to 0
-				if val == z_in1 and val == z_in2: # Both Z_in1/z_in2 are equal
-					print('Z_in1 and Z_in2 are equal, so update just z_in1')
-					# Update bias and weight values of z_in1
-					bias[a] = round(bias[a] + rate * (1 - z_in1), 2)
-					weights[a][a] = round(weights[a][a] + rate * (1 - z_in1) * x[i][a], 2)
-					weights[b][a] = round(weights[b][a] + rate * (1 - z_in1) * x[i][b], 2)
-					print('T = 1, Updated bias[0]: ', bias[a])
-					print('T = 1, Updated weights: ', weights)
+				# Get the possible changes
+				cb1 = upWeights(rate, 1, z_in1, 1)
+				cb2 = upWeights(rate, 1, z_in2, 1)
+				czin1 = upWeights(rate, 1, z_in1, x[i][a])
+				czin11 = upWeights(rate, 1, z_in1, x[i][b])
+				czin2 = upWeights(rate, 1, z_in2, x[i][a])
+				czin22 = upWeights(rate, 1, z_in2, x[i][b])
+				# Both Z_in1/z_in2 are equal
+				if val == z_in1 and val == z_in2: 
+					pick = randint(1, 2)
+					if pick == 1:
+						print('Z_in1 and Z_in2 are equal, so update just z_in1')
+						if czin1 < threshold and czin11 < threshold and cb1 < threshold:
+							print('Error is small enough')
+							converged += 1
+							print('converged equals: ', converged)
+							if converged == pairs:
+								print('We have converged')
+								stop = True
+								print('Training converged after {x} epochs'.format(x=era))
+								# Write to output file
+								with open(outfile, "w") as store:
+									store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+								store.close()
+							break
+							continue
+						else:
+							print('This is what the z_in1 weights will change by: ', czin1, czin11)
+							bias[a] = round(bias[a] + cb1)
+							weights[a][a] = round(weights[a][a] + czin1, 2)
+							weights[b][a] = round(weights[b][a] + czin11, 2)
+							if converged > 0: converged = 0
+							continue
+					else: # choose 2
+						print('zin1 and zin2 are equal, but update zin2')
+						if czin2 < threshold and czin22 < threshold and cb2 < threshold:
+							print('Error too small')
+							converged += 1
+							print('converged equals: ', converged)
+							if converged == pairs:
+								print('We have converged')
+								stop = True
+								print('Training converged after {x} epochs'.format(x=era))
+								# Write to output file
+								with open(outfile, "w") as store:
+									store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+								store.close()
+								break
+							continue
+						else: 
+							print('Zin1 Weights will change by: ', czin2, czin22)
+							bias[b] = round(bias[b] + cb2)
+							weights[a][b] = round(weights[a][b] + czin2, 2)
+							weights[b][b] = round(weights[b][b] + czin22, 2)
+							# reset converged
+							if converged > 0: converged = 0
+							continue
 				if val == z_in1: # Both Z_in1/z_in2 are equal
 					# Update bias and weight values of z_in1
-					bias[a] = round(bias[a] + rate * (1 - z_in1), 2)
-					weights[a][a] = round(weights[a][a] + rate * (1 - z_in1) * x[i][a], 2)
-					weights[b][a] = round(weights[b][a] + rate * (1 - z_in1) * x[i][b], 2)
-					print('T = 1, Updated bias[0]: ', bias[a])
-					print('T = 1, Updated weights: ', weights)
+					if czin1 < threshold and czin11 < threshold and cb1 < threshold:
+						print('Error is small enough')
+						converged += 1
+						print('converged equals: ', converged)
+						if converged == pairs:
+							print('We have converged')
+							stop = True
+							print('Training converged after {x} epochs'.format(x=era))
+							# Write to output file
+							with open(outfile, "w") as store:
+								store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+							store.close()
+							break
+						continue
+					else:
+						print('This is what the zin1 weights will change by: ', czin1, czin11)
+						weights[a][a] = round(weights[a][a] + czin1, 2)
+						weights[b][a] = round(weights[b][a] + czin11, 2)
+						# update bias
+						bias[a] = round(bias[a] + cb1)
+						if converged > 0: converged = 0
+						continue
 				if val == z_in2:
 					# Update the weight and bias corresponding to z2
-					bias[b] = round(bias[b] + rate * (1 - z_in2), 2)
-					weights[b][b] = round(weights[b][b] + rate * (1 - z_in2) * x[i][b], 2)
-					weights[a][b] = round(weights[a][b] + rate * (1 - z_in2) * x[i][a], 2)
-					print('T = 1, Updated bias[1]: ', bias[b])
-					print('T = 1, Updated weights: ', weights)
-				# Reset converged
-				if converged > 0: 
-					converged = 0
+					if czin2 < threshold and czin22 < threshold and cb2 < threshold:
+						print('Error is small enough')
+						converged += 1
+						print('converged equals: ', converged)
+						if converged == pairs:
+							print('We have converged')
+							stop = True
+							print('Training converged after {x} epochs'.format(x=era))
+							# Write to output file
+							with open(outfile, "w") as store:
+								store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+							store.close()
+							break
+						continue
+					else:
+						print('This is what the zin2 weights will change by: ', czin2, czin22)
+						bias[b] = round(bias[b] + cb2)
+						weights[a][b] = round(weights[a][b] + czin2, 2)
+						weights[b][b] = round(weights[b][b] + czin22, 2)
+						if converged > 0: converged = 0
+						continue
 			# t = -1 and z1 0r z2 = 1
-			elif t[i][a] == -1.0 and (z1 == 1 or z2 ==1):
+			elif t[i][a] == -1.0: 
+				# Get possible weight changes
+				cb1 = upWeights(rate, -1, z_in1, 1)
+				cb2 = upWeights(rate, -1, z_in2, 1)
+				czin1 = upWeights(rate, -1, z_in1, x[i][a])
+				czin11 = upWeights(rate, -1, z_in1, x[i][b])
+				czin2 = upWeights(rate, -1, z_in2, x[i][a])
+				czin22 = upWeights(rate, -1, z_in2, x[i][b])
+				if z1 == 1 and z2 == 1:
 				# Both z_in1 and z_in2 have positive inputs 
-				if z_in1 >= 0 and z_in2 >= 0:
-					print('both positive')
-					# update bias'
-					bias[a] = round(bias[a] + rate * (-1 - z_in1), 2)
-					bias[b] = round(bias[b] + rate * (-1 - z_in2), 2)
-					# update weights
-					weights[a][a] = round(weights[a][a] + rate * (-1 - z_in1) * x[i][a], 2)
-					weights[a][b] = round(weights[a][b] + rate * (-1 - z_in2) * x[i][a], 2)
-					weights[b][b] = round(weights[b][b] + rate * (-1 - z_in2) * x[i][b], 2)
-					weights[b][a] = round(weights[b][a] + rate * (-1 - z_in1) * x[i][b], 2)
+					if czin1 < threshold and czin11 < threshold and cb1 < threshold or czin2 < threshold and czin22 < threshold and cb2 < threshold:
+						print('Error is small enough')
+						converged += 1
+						print('converged equals: ', converged)
+						if converged == pairs:
+							print('We have converged')
+							stop = True
+							print('Training converged after {x} epochs'.format(x=era))
+							# Write to output file
+							with open(outfile, "w") as store:
+								store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+							store.close()
+							break
+						continue
+					else: # Not small enough
+						# update weights
+						weights[a][a] = round(weights[a][a] + czin1)
+						weights[a][b] = round(weights[a][b] + czin2)
+						weights[b][b] = round(weights[b][b] + czin22)
+						weights[b][a] = round(weights[b][a] + czin11)
+						# update bias
+						bias[a] = bias[a] + cb1
+						bias[b] = bias[b] + cb2
+						if converged > 0: converged = 0
+						continue
+				# Only z_in1 is positive
 				elif z_in1 >= 0:
 					print('z_in1 positive', z_in1)
-					# update bias
-					bias[a] = round(bias[a] + rate * (-1 - z_in1), 2)
-					# update corresponding weights
-					weights[a][a] = round(weights[a][a] + (rate * (-1 - z_in1) * x[i][a]), 2)
-					weights[b][a] = round(weights[b][a] + (rate * (-1 - z_in1) * x[i][b]), 2)
+					if czin1 < threshold and czin11 < threshold:
+						print('Error is small enough')
+						converged += 1
+						print('converged equals: ', converged)
+						if converged == pairs:
+							print('We have converged')
+							stop = True
+							print('Training converged after {x} epochs'.format(x=era))
+							# Write to output file
+							with open(outfile, "w") as store:
+								store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+							store.close()
+							break
+						continue
+					else: # Not small enough
+						# update weights
+						weights[a][a] = round(weights[a][a] + czin1)
+						weights[b][a] = round(weights[b][a] + czin11)
+						# update bias
+						bias[a] = bias[a] + cb1
+						if converged > 0: converged = 0
+						continue
 				elif z_in2 >= 0: # just z_in2 is greater than 0
 					print('z_in2 positive', z_in2)
-					#update bias
-					bias[b] = round(bias[b] + rate * (-1 - z_in2), 2)
-					# update weights
-					weights[a][b] = round(weights[a][b] + (rate * (-1 - z_in2) * x[i][a]), 2)
-					weights[b][b] = round(weights[b][b] + (rate * (-1 - z_in2) * x[i][b]), 2)
-				# Reset converged
-				if converged > 0: 
-					converged = 0
+					if czin2 < threshold and czin22 < threshold and cb2 < threshold:
+						print('Error is small enough')
+						converged += 1
+						print('converged equals: ', converged)
+						if converged == pairs:
+							print('We have converged')
+							stop = True
+							print('Training converged after {x} epochs'.format(x=era))
+							# Write to output file
+							with open(outfile, "w") as store:
+								store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
+							store.close()
+							break
+						continue
+					else: # not small enough
+						weights[a][b] = round(weights[a][b] + czin2)
+						weights[b][b] = round(weights[b][b] + czin22)
+						# update bias
+						bias[b] = bias[b] + cb2
+						if converged > 0: converged = 0
+						continue
 		# Check if we've gone through the number of training pairs
 		if count == pairs:
 			era += 1 # increment epoch we're on
@@ -158,14 +302,13 @@ def trainAlgo(tsets, bias, rate, epochs, weights, pairs, inputs, outfile, output
 		# Check if the era is greater than epochs so we can stop
 		if era >= epochs:
 			stop = True
-			print('Training converged after {x} epochs'.format(x=epochs+1))
+			print('Training converged after {x} epochs'.format(x=era))
 			# Write to output file
 			with open(outfile, "w") as store:
-				store.write("%d\n%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, outputs, pairs, weights, bias, v1, v2))
+				store.write("%d\n%d\n\n%s\n%s\n%.2f\n%.2f" %(inputs, pairs, weights, bias, v1, v2))
 			store.close()
 			return outfile
-			break
-
+			
 """
 Activation Function for Madaline
 
@@ -181,8 +324,8 @@ def activateF(z_in):
 	else: return -1
 
 """
-Helper function to read in the .dat file and store the 
-	data inside of it
+Helper function to read in the .txt file and store the 
+	data inside of it (only works for training text file)
 
 arg: aFile - file to be read
 """
@@ -246,16 +389,16 @@ def howLong():
 """
 Function to get the learning rate for NN
 """
-def howFast():
+def learnRate():
 	try:
 		# Get learning rate alpha
 		rate = float(input("\nEnter learning rate alpha from 0 to 1 (not including 0):\n>>  "))
 		if not 0 < rate <= 1:
 			print("Your alpha value needs to be between 0 and 1, but not including 0.\n>>")
-			howFast()
+			learnRate()
 	except ValueError:
 		print('The learning rate needs to be a decimal between 0 (but not including 0) and 1')
-		howFast()
+		learnRate()
 	return rate
 
 """
@@ -264,7 +407,7 @@ Function to get the output file for either the training or testing file
 def storeMe():
 	try:
 		# Get output file name for results
-		outfile = input("Enter the file name where the results will be saved: \n>>")
+		outfile = input("\nEnter the file name where the results will be saved: \n>>")
 		if os.path.exists(outfile) and not os.stat(outfile).st_size == 0:
 			print('Try again, that file is not empty') 
 			storeMe() 
@@ -277,20 +420,99 @@ def storeMe():
 	return outfile
 
 """
+Function to get the testing file for the NN
+"""
+def getMe():
+	try:
+		# Get testing file for NN
+		tFile = input("\nEnter the file name of the inputs you'd like to test: \n>>")
+		# Check if file doesn't exist or it's empty
+		if not os.path.exists(tFile) or not os.path.getsize(tFile) > 0:
+			print('The file entered needs to be a non-empty file')
+			getMe()
+		# File doesn't end with .txt
+		if not tFile.endswith('.txt'):
+			print('The file needs to be a text file')
+			getMe()
+	except ValueError:
+		print('Please enter a non-empty testing file')
+		getMe()
+	return tFile
+
+"""
+Function to get the threshold from the user for checking when we've converged
+"""
+def errorDiff():
+	try:
+		threshold = float(input('\nPlease enter a threshold to check for convergence: \n>> '))
+		if threshold > 0.1:
+			print('Please choose a threshold between 0 and 0.1')
+			errorDiff()
+	except ValueError():
+		print('Please enter a decimal between 0 and 0.1')
+		errorDiff()
+	return threshold
+
+"""
 Function to gather the weights, epochs, rates and output file name 
-	for Madaline NN 
+	for training Madaline NN 
 """
 def initializeIt():
 	try:
 		# Get and store weights, epochs, rates and the name of the output file
 		weights = gimmeWeights()
 		epochs = howLong()
-		rate = howFast()
+		threshold = errorDiff()
+		rate = learnRate()
 		outfile = storeMe()
 	except KeyboardInterrupt:
 		sys.exit()
-	return weights, epochs, rate, outfile
+	return weights, epochs, threshold, rate, outfile
 
+"""
+Function to read and store the values of testing file
+
+arg: 
+	testfile - file to read and store the inputs and training sets
+"""
+def read2Store(testfile):
+	try:
+		tests = []
+			# Open and store contents of data file
+		with open(testfile) as t_open:
+			contents = t_open.readlines()
+		# Read through specific lines to store input, pairs and testing sets
+		for i, line in enumerate(contents):
+			if i == 0:
+				t_ins = line
+				t_ins = int(t_ins)
+			elif i == 1:
+				t_pairs = line
+				t_pairs = int(t_pairs)
+			# Dealing with the samples
+			else:
+				x = line.strip()
+				tests.append(x.split())
+	except TypeError:
+		print("Invalid File")
+		os.system('clear')
+		mm.menu()
+	return t_ins, t_pairs, tests
+
+"""
+Function to get both the testing file and the trained NN weights
+"""
+def testIt():
+	try:
+		# Gets the test file and the inputs, pairs and tsetset
+		tstfile = getMe()
+		ins, pairs, tstset = read2Store(tstfile)
+		# Get empty output file for the results
+		outfile = storeMe()
+	except KeyboardInterrupt:
+		sys.exit()
+	return outfile, ins, pairs, tstset
+	# return tstfile, outfile
 """
 Function to test the trained NN
 
@@ -300,7 +522,15 @@ arg:
 	bias - weights of the bias' from the trained NN
 	v1/v2 - the weights going into the output layer from the hidden layer
 """
-# def testNN(weights, tstset, bias, v1, v2):
+def testNN(weights, tstset, bias, v1, v2):
+	x = []
+	# Store each training pair, s:t 
+	for tset in tstset:
+		if tset != '':
+			x.append(tset)
+	
+
+
 
 def main():
 	# Greet/get initial input file
@@ -317,14 +547,19 @@ def main():
 			if choice == 1:
 				val = mm.pick_one(choice) 
 				# Store values from initilizeIt()
-				weights, epochs, rate, outfile = val
+				weights, epochs, threshold, rate, outfile = val
 				t = Training(ins, outs, pairs, weights, tsets)
 				# Store output file from training
-				of = trainAlgo(t.tset, t.bias, rate, epochs, t.weights, t.pairs, t.inputs, outfile, t.outputs)
+				of = trainAlgo(t.tset, t.bias, rate, epochs, t.weights, t.pairs, t.inputs, outfile, threshold)
 				continue
 			# Testing the NN
-			elif choice == 2: 
+			if choice == 2: 
+				# Store values from testIt
 				val = mm.pick_one(choice)
+				ofile, ins, pairs, tstset = val
+				print('I get here')
+				testNN(0, tstset, 0, 0, 0)
+				#continue
 				# outF = storeMe() # Get output file to store the results
 			# mm.pick_one(choice)
 			break
